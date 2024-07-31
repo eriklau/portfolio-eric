535825f7ef6e586003b83d7ff06d2a0fe33f2bc0
Eric Liu <liu.yanqi26@gmail.com>
2024-07-30 20:38:25 -0400

    Remove functions dir and update contact form URL

--- public/script.js
+++ public/script.js
@@ -2,45 +2,43 @@ const menu = document.querySelector("#menu");
 const nav = document.querySelector(".links");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    menu.classList.toggle('active');
+  menu.classList.toggle('bx-x');
+  menu.classList.toggle('active');
 }
 
-// Function to handle form submission via AJAX
 function submitForm() {
-    const form = document.getElementById('contact-form');
-    const formData = new FormData(form);
+  const form = document.getElementById('contact-form');
+  const formData = new FormData(form);
 
-    console.log('Submitting form with data:');
-    console.log('Full Name:', formData.get('fullname'));
-    console.log('Email:', formData.get('email'));
-    console.log('Message:', formData.get('message'));
+  console.log('Submitting form with data:');
+  console.log('Full Name:', formData.get('fullname'));
+  console.log('Email:', formData.get('email'));
+  console.log('Message:', formData.get('message'));
 
-    fetch('/contact', {
-        method: 'POST',
-        body: JSON.stringify({
-            fullname: formData.get('fullname'),
-            email: formData.get('email'),
-            message: formData.get('message')
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.text())
-    .then(data => {
-        console.log('Form submission response:', data);
-        alert(data); // Display success message
-        form.reset(); // Clear the form
-    })
-    .catch(error => {
-        console.error('Error submitting form:', error);
-        alert('There was an error submitting the form.');
-    });
+  fetch('https://website-eric.onrender.com/contact', {
+    method: 'POST',
+    body: JSON.stringify({
+      fullname: formData.get('fullname'),
+      email: formData.get('email'),
+      message: formData.get('message')
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log('Form submission response:', data);
+    alert(data);
+    form.reset();
+  })
+  .catch(error => {
+    console.error('Error submitting form:', error);
+    alert('There was an error submitting the form.');
+  });
 }
 
-// Attach the submitForm function to the form's submit event
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    submitForm(); // Call the submitForm function
+  event.preventDefault();
+  submitForm();
 });
